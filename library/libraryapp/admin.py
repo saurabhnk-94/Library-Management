1203f0ea0aedbdf42ba582591e1907c0da81b437
saurabhnayak <saurabhnk94@gmail.com>
2019-10-12 16:18:04 +0530

    Updating the library management

--- library/libraryapp/admin.py
+++ library/libraryapp/admin.py
@@ -2,18 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book, Author
-
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('book_name','book_price','availability','upper_name','isbn','author_name')
-    list_filter = ('availability',)
-    search_fields = ['book_name','book_price','author_name__author_fname']
+from .models import Book,Author,Member
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('author_fname','author_lname','full_name')
+    list_display = ('author_name','author_mail')
+    search_fields = ['author_name']
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('book_name','availability','isbn','author_obj__author_name')
+    list_filter = ('availability',)
+    search_fields = ['book_name']
+
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('member_id','member_name','contact')
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register()
 
--- library/libraryapp/models.py
+++ library/libraryapp/models.py
@@ -1,29 +1,39 @@
 from django.db import models
 
+
 class Author(models.Model):
-    author_fname = models.CharField(max_length = 200)
-    author_lname = models.CharField(max_length=200)
+    author_name = models.CharField(max_length=200)
+    author_mail = models.EmailField()
 
     def __str__(self):
-        return self.author_fname +' '+self.author_lname
-
-    def full_name(self):
-        return '{} {}'.format(self.author_fname,self.author_lname).upper()
+        return self.author_name +'-'+ self.author_mail
 
 
 class Book(models.Model):
-    book_name = models.CharField(max_length = 200)
-    book_price = models.IntegerField(default = 0)
-    availability = models.BooleanField(default = True)
-    isbn = models.CharField(max_length = 100)
-    author_name = models.ForeignKey(Author, null=True, on_delete =models.CASCADE)
-
-
+    book_name = models.CharField(max_length=200)
+    availability = models.BooleanField(default=True)
+    isbn = models.CharField(max_length=100)
+    author_obj = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.book_name + '-' + str(self.book_price) + self.isbn
+        return self.book_name + '-' + self.isbn
 
-    def upper_name(self):
-        return self.book_name.upper()
 
-# Create your models here.
+class Member(models.Model):
+    member_id = models.AutoField(primary_key=True)
+    member_name = models.CharField(max_length=200)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.member_name + '-' + str(self.contact)
+
+# class Records(models.Model):
+#     book_object = models.ForeignKey(Book, null = True, on_delete = models.CASCADE)
+#     issue_date = models.DateField()
+#     return_date = models.DateField()
+#
+#
+#     def book_details(self):
+#         return
+#
+
