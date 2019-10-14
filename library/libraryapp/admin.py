aed9407011080489ad9898b1b06f00c628d80702
saurabhnayak <saurabhnk94@gmail.com>
2019-10-14 18:20:37 +0530

    Library app with records

--- library/libraryapp/admin.py
+++ library/libraryapp/admin.py
@@ -2,24 +2,35 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book,Author,Member
+from .models import *
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('author_name','author_mail')
+    ordering = ('author_name',)
     search_fields = ['author_name']
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('book_name','availability','isbn','author_obj__author_name')
+    list_display = ('book_name','availability','isbn','author_name','total_books','stock')
+    ordering = ('book_name',)
     list_filter = ('availability',)
     search_fields = ['book_name']
 
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('member_id','member_name','contact')
+    list_display = ('member_id','member_name','contact','member_mail')
+    ordering = ('member_name',)
+    search_fields = ['member_name']
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('book','issue_date','return_date', 'member_name','returned',)
+    ordering = ('book',)
+    search_fields = ['book__book_name',]
+
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register()
-
+admin.site.register(Member, MemberAdmin)
+admin.site.register(Record, RecordAdmin)
--- library/libraryapp/models.py
+++ library/libraryapp/models.py
@@ -1,39 +1,70 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save,pre_save
 
 
 class Author(models.Model):
     author_name = models.CharField(max_length=200)
-    author_mail = models.EmailField()
+    author_mail = models.EmailField(default = False)
 
     def __str__(self):
-        return self.author_name +'-'+ self.author_mail
+        return self.author_name
 
 
 class Book(models.Model):
     book_name = models.CharField(max_length=200)
     availability = models.BooleanField(default=True)
     isbn = models.CharField(max_length=100)
-    author_obj = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    author_name = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
+    stock = models.IntegerField(default = 1)
+    total_books = models.IntegerField(default = 1)
 
     def __str__(self):
-        return self.book_name + '-' + self.isbn
+        return self.book_name
 
 
 class Member(models.Model):
     member_id = models.AutoField(primary_key=True)
     member_name = models.CharField(max_length=200)
     contact = models.IntegerField()
+    member_mail = models.EmailField()
 
     def __str__(self):
-        return self.member_name + '-' + str(self.contact)
+        return self.member_name
 
-# class Records(models.Model):
-#     book_object = models.ForeignKey(Book, null = True, on_delete = models.CASCADE)
-#     issue_date = models.DateField()
-#     return_date = models.DateField()
-#
-#
-#     def book_details(self):
-#         return
-#
+    def name(self):
+        return self.member_name
+
+
+class Record(models.Model):
+    book = models.ForeignKey(Book, on_delete= models.DO_NOTHING)
+    issue_date = models.DateField()
+    return_date = models.DateField()
+    member = models.ForeignKey(Member, on_delete = models.DO_NOTHING)
+    returned = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.book.book_name
+
+    def member_name(self):
+        return self.member.member_name
+
+@receiver(post_save, sender = Record)
+def update_stock(sender,instance, created,**kwargs):
+    book = instance.book
+    if created:
+        book.stock = book.stock - 1
+        if book.stock == 0:
+            book.availability = False
+        book.save()
+
+@receiver(post_save, sender = Record)
+def increment_stock(sender,instance,**kwargs):
+    book = instance.book
+    if instance.returned:
+        book.stock = book.stock + 1
+        book.availability = True
+        book.save()
+
+
 
