0d46a4bcc4a0297cc366574fec2b997821a72a08
saurabhnayak <saurabhnk94@gmail.com>
2019-10-15 17:50:43 +0530

    Library app updation using forms

--- library/libraryapp/admin.py
+++ library/libraryapp/admin.py
@@ -1,14 +1,43 @@
 from django.contrib import admin
-
+from django import forms
 # Register your models here.
 
 from .models import *
+
+class AuthorAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AuthorAdminForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        author_name = self.cleaned_data.get('author_name')
+        if len(author_name) < 4:
+            raise forms.ValidationError('Author name should not be less that 4 characters',code='invalid')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(BookAdminForm, self).save(commit=commit)
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('author_name','author_mail')
     ordering = ('author_name',)
     search_fields = ['author_name']
+    form = AuthorAdminForm
+
+
+class BookAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BookAdminForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        isbn = self.cleaned_data.get('isbn')
+        if len(isbn) < 10:
+            raise forms.ValidationError('ISBN is not Valid, should be more that 10 chararcters',code='invalid')
+
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(BookAdminForm, self).save(commit=commit)
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -16,18 +45,50 @@ class BookAdmin(admin.ModelAdmin):
     ordering = ('book_name',)
     list_filter = ('availability',)
     search_fields = ['book_name']
+    form = BookAdminForm
+
+
+class MemberAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(MemberAdminForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        contact = self.cleaned_data.get('contact')
+        if len(str(contact)) != 10:
+            raise forms.ValidationError("Please Enter the Valid Phone Number which is Of 10 digits", code='invalid')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(MemberAdminForm, self).save(commit=commit)
 
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('member_id','member_name','contact','member_mail')
+    list_display = ('member_name','contact','member_mail','member_address')
     ordering = ('member_name',)
     search_fields = ['member_name']
+    form = MemberAdminForm
+
+
+class RecordAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RecordAdminForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        book = self.cleaned_data.get('book')
+        if book.stock == 0:
+            if self.cleaned_data.get('returned') == False:
+                raise forms.ValidationError('Book is out of stock',code='invalid')
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        return super(RecordAdminForm, self).save(commit=commit)
 
 
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('book','issue_date','return_date', 'member_name','returned',)
     ordering = ('book',)
     search_fields = ['book__book_name',]
+    form = RecordAdminForm
 
 
 admin.site.register(Book, BookAdmin)
--- library/libraryapp/models.py
+++ library/libraryapp/models.py
@@ -28,20 +28,18 @@ class Member(models.Model):
     member_name = models.CharField(max_length=200)
     contact = models.IntegerField()
     member_mail = models.EmailField()
+    member_address = models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return self.member_name
 
-    def name(self):
-        return self.member_name
-
 
 class Record(models.Model):
-    book = models.ForeignKey(Book, on_delete= models.DO_NOTHING)
+    book = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
     issue_date = models.DateField()
     return_date = models.DateField()
-    member = models.ForeignKey(Member, on_delete = models.DO_NOTHING)
-    returned = models.BooleanField(default = False)
+    member = models.ForeignKey(Member, on_delete=models.DO_NOTHING)
+    returned = models.BooleanField(default=False)
 
     def __str__(self):
         return self.book.book_name
@@ -49,22 +47,34 @@ class Record(models.Model):
     def member_name(self):
         return self.member.member_name
 
-@receiver(post_save, sender = Record)
+
+@receiver(post_save, sender=Record)
 def update_stock(sender,instance, created,**kwargs):
     book = instance.book
     if created:
-        book.stock = book.stock - 1
+        if book.availability == True:
+            book.stock = book.stock - 1
         if book.stock == 0:
             book.availability = False
         book.save()
 
-@receiver(post_save, sender = Record)
+
+@receiver(pre_save, sender=Record)
 def increment_stock(sender,instance,**kwargs):
-    book = instance.book
-    if instance.returned:
-        book.stock = book.stock + 1
-        book.availability = True
-        book.save()
+    if not instance.returned:
+        if any(Record.objects.filter(id=instance.id)):
+            if Record.objects.get(id=instance.id).returned:
+                instance.returned = True
+    else:
+        if Record.objects.get(id=instance.id).returned == False:
+            book = instance.book
+            book.stock = book.stock + 1
+            if book.stock > 0:
+                book.availability = True
+            book.save()
+
+
+
 
 
 
